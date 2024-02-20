94ac3f3454b063fe10aabf7366de664fd8011e6f
Antoine <a.delubriac@hotmail.com>
2024-02-20 14:19:49 +0100

    exo1 maj

--- exo1.php
+++ exo1.php
@@ -1,5 +1,23 @@
 <h1>Exercice 1</h1>
 
-<p>Enoncé de l'exercice</p>
+<p>
+Soit la phrase « Notre formation DL commence aujourd'hui ».
+Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase 
+(espaces inclus).
+</p>
+
 
 <?php
+
+// déclaration variable pour ma phrase :
+$maPhrase = "Notre formation DL commence aujourd'hui";
+
+// Calcul du nombre de caratères : 
+$nbCaractères = strlen($maPhrase)
+?>
+
+<h2>Résultat</h2>
+<?php
+echo "La phrase « $maPhrase » contient $nbCaractères caractères<br>";
+?>
+
