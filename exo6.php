d0c1a86dd144a0008898a08945ffdea64e8e5e9e
Antoine <a.delubriac@hotmail.com>
2024-02-21 11:44:12 +0100

    Maj exercices

---
+++ exo6.php
@@ -0,0 +1,39 @@
+<h1>Exercice 6</h1>
+
+<p>
+Ecrire un algorithme permettant de calculer un montant de facture à régler à partir de la quantité 
+d'articles, son prix hors taxe et un taux de TVA (exprimé en décimal. Ex : 20 % -> 0.2)
+</p>
+
+<!-- 
+Affichage :
+Prix unitaire de l’article : 9.99 €
+Quantité : 5
+Taux de TVA : 0.2
+Le montant de la facture à régler est de : 59.94 €
+ -->
+
+ <h2>Résultat</h2>
+
+ <?php
+
+// Prix unitaire de l'article : 
+$prixArticle = 9.99;
+
+// Quantité d'articles : 
+$quantité = 5;
+
+// TVA :
+$tva = 0.2;
+
+// Calcul de la facture H.T :
+$sommeHt = $prixArticle * $quantité;
+
+// Ajout de le TVA :
+$sommeTtc = $sommeHt + ($sommeHt * $tva);
+
+// Affichage finale : 
+echo "Prix unitaire de l'article : $prixArticle €<br>";
+echo "Quantité : $quantité<br>";
+echo "Taux de TVA : $tva<br>";
+echo "Le montant de la facture à régler est de : $sommeTtc €";
---
+++ exo7.php
@@ -0,0 +1,46 @@
+<h1>Exercice 7</h1>
+
+<p>
+Ecrire un algorithme permettant de renvoyer la catégorie d'un enfant en fonction de son âge :
+    Poussin : entre 6 et 7 ans
+    Pupille : entre 8 et 9 ans
+    Minime : entre 10 et 11 ans
+    Cadet : à partir de 12 ans
+Si la catégorie n'est pas gérée, merci de le préciser.
+</p>
+
+<!-- 
+Affichage : 
+L’enfant qui a 10 ans appartient à la catégorie « Minime »
+-->
+
+<h2>Résultat</h2>
+
+<?php
+
+// Age enfant : 
+$ageEnfant = 10;
+
+// Catégorie :
+$categorie = "";
+
+// Phrase d'affichage :
+$debutPhrase = "L'enfant qui a";
+$finPhrase = "appartient à la catégorie";
+
+// Les catégories d'age :
+if ($ageEnfant >= 6 && $ageEnfant <= 7) {
+    $categorie = "« Poussin »";
+} elseif ($ageEnfant >= 8 && $ageEnfant <= 9) {
+    $categorie = "« Pupille »" ;
+} elseif ($ageEnfant >= 10 && $ageEnfant <= 11) {
+    $categorie = "« Minime »";
+} elseif ($ageEnfant >= 12 && $ageEnfant < 18) {
+    $categorie = "« Cadet »";
+} else {
+    $categorie = "« Indéfinie »";
+}
+$indefinie = ($ageEnfant < 5 || $ageEnfant > 12);
+
+// Affichage finale en fonction de la catégorie : 
+echo "$debutPhrase $ageEnfant $finPhrase $categorie";
