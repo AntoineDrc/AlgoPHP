93c3ed46c44c2f78e9c2170952743ac63faf85c2
Antoine <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-02-26 16:44:46 +0100

    Exo 12 AlgoPHP

---
+++ exo12.php
@@ -0,0 +1,53 @@
+<h1>Exercice 12</h1>
+
+<p>
+A partir d'une fonction personnalisée et à partir d'un tableau de prénoms et de langue associée 
+(tableau contenant clé et valeur), dire bonjour aux différentes personnes dans leur langue 
+respective (français ➔ « Salut », anglais ➔ « Hello », espagnol ➔ « Hola »)
+Exemple : tableau ➔ Mickaël -> FRA, Virgile -> ESP, Marie-Claire -> ENG <br><br>
+
+<!-- 
+    Affichage :
+Salut Mickaël
+Hola Virgile
+Hello Marie-Claire
+ -->
+
+Variante : trier d'abord le tableau par ordre alphabétique du prénom
+
+<!-- 
+Affichage :
+Hello Marie-Claire
+Salut Mickaël
+Hola Virgile
+ -->
+ </p>
+
+
+ <h2>Résultat</h2>
+
+ <?php
+
+// Création d'un tableau associatif : 
+$prenomLangue = ["Mickaël" => "FRA", "Virgile" => "ESP", "Marie-Claire" => "ENG",];
+
+// Création d'une boucle : 
+foreach ($prenomLangue as $prenom => $lang) { 
+    // $prenomLangue = tableau
+    // $prenom = valeur de la clé
+    // $lang = la valeur associée à la clé
+    
+// Création de la condition :
+    switch($lang){
+        case "FRA";
+            echo"Bonjour " . $prenom . "<br>";
+            break;
+        case "ESP";
+            echo"Hola " . $prenom . "<br>";
+            break;
+        case "ENG";
+            echo"Hello " . $prenom . "<br>";
+// switch est une altérnative à if-else. On pourrait le traduire par : "Dans le cas où".
+
+    }
+}
