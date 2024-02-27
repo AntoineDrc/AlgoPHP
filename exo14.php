8a16cb414c2479e838684da6a8a4840c2a4b252c
Antoine <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-02-27 14:39:35 +0100

    fin exercice 14

--- exo14.php
+++ exo14.php
@@ -11,3 +11,18 @@ Age de la personne : 33 ans 4 mois 4 jours
  -->
 
  <h2>Résultat</h2>
+
+ <?php
+
+// Variable à la date du 21/05/2018 :
+$date1 = new DateTime("2018-05-21");
+
+// Variable date de naissance de la personne : 
+$dateN = new DateTime("1985-01-17");
+
+// Calcul de l'âge en fonction de la différence : 
+$age = date_diff($date1, $dateN); // date_diff est une fonction qui permet de calculer la différence entre deux objets DateTime.
+
+// Affichage de la date de naissance ainsi que l'âge : 
+echo $dateN->format('d/m/Y') . " :" . "<br>";
+echo "Age de la personne : " . $age->format('%y ans %m mois %d jours');
